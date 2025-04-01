4dfee6085a05718089611e37eec9323b288d279a
vivek shah <vivek.shah@seaflux.tech>
2025-04-01 16:10:48 +0530

    refactor: add enum in order-status and format the code

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -29,25 +29,27 @@ export class AuthController {
     InjectRepositories(this);
   }
 
-  //signup user
+  // Signup user
   public PostSignup = async (req: TRequest<SignupDto>, res: TResponse): Promise<void> => {
     req.dto.password = await Bcrypt.hash(req.dto.password);
 
     try {
       const user = this.userRepository.create(req.dto);
       await this.userRepository.save(user);
+
       res.status(200).json({ success: true, user: user });
-      return;
     } catch (err: unknown) {
       const error = err as CustomError;
+
       if (!error.statusCode) {
         error.statusCode = 500;
       }
+
       res.status(error.statusCode).json({ error: error });
     }
   };
 
-  //login with email or phone
+  // Login with email or phone
   public PostLogin = async (req: TRequest<LoginDto>, res: TResponse): Promise<void> => {
     const { email, phone, password } = req.dto;
 
@@ -57,6 +59,7 @@ export class AuthController {
         error.statusCode = 400;
         throw error;
       }
+
       const user = await this.userRepository.findOne({
         where: { ...(email ? { email } : { phone }) },
       });
@@ -66,64 +69,75 @@ export class AuthController {
         error.statusCode = 404;
         throw error;
       }
+
       const compare = await Bcrypt.verify(password, user.password);
+
       if (!compare) {
         res.status(400).json({ message: "Password not matched" });
         return;
       }
 
       const token = JwtHelper.encode({ id: user.id });
+
       res.status(200).json({ success: true, token: token });
-      return;
     } catch (err: unknown) {
       const error = err as CustomError;
+
       if (!error.statusCode) {
         error.statusCode = 500;
       }
+
       res.status(error.statusCode).json({ error: error });
     }
   };
 
-  //forgot password via email
+  // Forgot password via email
   public forgotPassword = async (req: TRequest<ForgotPasswordDto>, res: TResponse): Promise<void> => {
     const { email } = req.dto;
+
     try {
       crypto.randomBytes(32, async (error, buffer) => {
         if (error) {
-          res.status(400).json({ message: "error!" });
+          res.status(400).json({ message: "Error generating token!" });
+          return;
         }
+
         const token = buffer.toString("hex");
+
         const user = await this.userRepository.findOne({ where: { email } });
+
         if (!user) {
           const error = new Error("User not found!") as CustomError;
           error.statusCode = 404;
           throw error;
         }
+
         user.resetToken = token;
         user.resetTokenExpiration = new Date(Date.now() + 3600000);
         await this.userRepository.save(user);
 
-        const sendEmail = await transporter.sendMail({
+        await transporter.sendMail({
           from: "earl.hirthe@ethereal.email",
           to: req.body.email,
           subject: "Reset Password link",
-          html: `<h1>You TRequested a password reset</h1>
-        <p><a href="https:localhost:3000/reset/${token}">to set a new password</a></p>
-        `,
+          html: `<h1>You Requested a password reset</h1>
+          <p><a href="https://localhost:3000/reset/${token}">Set a new password</a></p>`,
         });
+
         res.status(200).json({ success: true });
-        return;
       });
     } catch (err: unknown) {
       const error = err as CustomError;
+
       if (!error.statusCode) {
         error.statusCode = 500;
       }
+
       res.status(error.statusCode).json({ error: error });
     }
   };
 
-  //set new password
+  // Set new password
   public NewPassword = async (req: TRequest<NewPasswordDto>, res: TResponse): Promise<void> => {
     const { newPassword, passwordToken } = req.dto;
 
@@ -134,52 +148,62 @@ export class AuthController {
           resetTokenExpiration: MoreThanOrEqual(new Date()),
         },
       });
+
       if (!user) {
-        const error = new Error("User not found!!") as CustomError;
+        const error = new Error("User not found!") as CustomError;
         error.statusCode = 404;
         throw error;
       }
-      const hashedPassword = await Bcrypt.hash(newPassword);
-      user.password = hashedPassword;
+
+      user.password = await Bcrypt.hash(newPassword);
       user.resetToken = null;
       user.resetTokenExpiration = null;
       await this.userRepository.save(user);
+
       res.status(200).json({ success: true, user: user });
     } catch (err: unknown) {
       const error = err as CustomError;
+
       if (!error.statusCode) {
         error.statusCode = 500;
       }
+
       res.status(error.statusCode).json({ error: error });
     }
   };
 
-  //update the user profile
+  // Update user profile
   public updateProfile = async (req: TRequest<UpdateUserDto>, res: TResponse): Promise<void> => {
     const userId = req.user.id;
     const { name, email, phone, password } = req.dto;
+
     try {
       const user = await this.userRepository.findOneById(userId);
+
       if (!user) {
-        const error = new Error("no user found") as CustomError;
+        const error = new Error("No user found") as CustomError;
         error.statusCode = 404;
         throw error;
       }
-      user.name = name || user?.name;
+
+      user.name = name || user.name;
       user.email = email || user.email;
       user.phone = phone || user.phone;
+
       if (password) {
-        const hashedPassword = await Bcrypt.hash(password);
-        user.password = hashedPassword;
+        user.password = await Bcrypt.hash(password);
       }
-      this.userRepository.save(user);
+
+      await this.userRepository.save(user);
+
       res.status(200).json({ success: true, user: user });
-      return;
     } catch (err: unknown) {
       const error = err as CustomError;
+
       if (!error.statusCode) {
         error.statusCode = 500;
       }
+
       res.status(error.statusCode).json({ error: error });
     }
   };
--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,6 +1,6 @@
 import { CartEntity, CartItemsEntity, OrderEntity, OrderItemEntity } from "@entities";
 import { InitRepository, InjectRepositories } from "@helpers";
-import { TRequest, TResponse } from "@types";
+import { enums, TRequest, TResponse } from "@types";
 import { In, Repository } from "typeorm";
 
 interface CustomError extends Error {
@@ -23,18 +23,21 @@ export class OrderController {
   constructor() {
     InjectRepositories(this);
   }
-  // create order
+
+  // Create Order
   public postOrder = async (req: TRequest, res: TResponse) => {
     try {
       // Find the cart for the current user
       const cart = await this.cartRepository.findOne({
         where: { userId: req.user.id },
       });
+
       if (!cart) {
         const error = new Error("No Cart Found") as CustomError;
         error.statusCode = 404;
         throw error;
       }
+
       const cartItems = await this.cartitemsRepository.find({
         where: { cart: { id: cart.id } },
         relations: ["product"],
@@ -45,35 +48,44 @@ export class OrderController {
         error.statusCode = 400;
         throw error;
       }
-      const order = await this.orderRepository.create({
-        userId: req.user.id,
-        totalAmount: cartItems.reduce((acc: number, item: any) => {
-          return acc + item.quantity * item.product.price;
-        }, 0),
 
+      const totalAmount = cartItems.reduce((acc: number, item: any) => {
+        return acc + item.quantity * item.product.price;
+      }, 0);
+
+      const order = this.orderRepository.create({
+        userId: req.user.id,
+        totalAmount,
         isCancelled: false,
-        status: "Pending",
+        status: enums.OrderStatus.Pending,
       });
+
       await this.orderRepository.save(order);
-      const orderItems = await cartItems.map((item: any) => ({
+
+      const orderItems = cartItems.map((item: any) => ({
         orderId: order.id,
         productId: item.productId,
         quantity: item.quantity,
         price: item.product.price,
       }));
+
       await this.orderItemRepository.insert(orderItems);
+
       await this.cartitemsRepository.remove(cartItems);
-      res.status(200).json({ success: true, order: order });
+
+      res.status(200).json({ success: true, order });
     } catch (err: unknown) {
       const error = err as CustomError;
+
       if (!error.statusCode) {
         error.statusCode = 500;
       }
-      res.status(error.statusCode).json({ error: error });
+
+      res.status(error.statusCode).json({ error });
     }
   };
 
-  // getOrders
+  // Get Orders
   public getOrder = async (req: TRequest, res: TResponse): Promise<void> => {
     try {
       const orders = await this.orderRepository.find({
@@ -110,29 +122,36 @@ export class OrderController {
       res.status(200).json({ success: true, orders: formattedOrders });
     } catch (err: unknown) {
       const error = err as CustomError;
+
       if (!error.statusCode) {
         error.statusCode = 500;
       }
-      res.status(error.statusCode).json({ error: error });
+
+      res.status(error.statusCode).json({ error });
     }
   };
 
-  //get order details and status
+  // Get Order Details and Status
   public orderDetails = async (req: TRequest, res: TResponse): Promise<void> => {
     try {
       const orderId = Number(req.params.orderId);
       const action = req.query.action;
 
       if (action === "cancel") {
-        await this.orderRepository.update(orderId, { isCancelled: true, status: "Cancelled" });
+        await this.orderRepository.update(orderId, {
+          isCancelled: true,
+          status: enums.OrderStatus.Cancelled,
+        });
       }
 
       const order = await this.orderRepository.findOne({ where: { id: orderId } });
+
       if (!order) {
         const error = new Error("No orders yet") as CustomError;
         error.statusCode = 404;
         throw error;
       }
+
       const orderDetails = await this.orderItemRepository.find({
         where: { order: { id: orderId } },
         select: ["productId", "quantity"],
@@ -147,10 +166,12 @@ export class OrderController {
       res.status(200).json({ success: true, orderDetails, orderStatus });
     } catch (err: unknown) {
       const error = err as CustomError;
+
       if (!error.statusCode) {
         error.statusCode = 500;
       }
-      res.status(error.statusCode).json({ error: error });
+
+      res.status(error.statusCode).json({ error });
     }
   };
 }
